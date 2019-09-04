4bff13cfa1e35abbf9b45fed7c9509c4f78955a5
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-04 13:44:01 -0600

    finished and tested logger custom middleware

--- api/server.js
+++ api/server.js
@@ -2,14 +2,20 @@ const express = require('express');
 
 const server = express();
 
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.url} at [${new Date().toISOString()}] `)
+
+  next()
+}
+
+server.use(express.json())
+server.use(logger)
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
 
 module.exports = server;
