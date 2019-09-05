e6fe7ff595b02c5b1c5d48cac0ff9fb2de7fe8e3
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-05 14:04:14 -0600

    added postRouter to server.js

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 console.log('environment', process.env.NODE_ENV)
 
 const userRouter = require('../users/userRouter')
+const postRouter = require('../posts/postRouter')
 
 const server = express();
 
@@ -16,9 +17,10 @@ server.use(express.json())
 server.use(logger)
 
 server.use('/users', userRouter)
+server.use('/posts', postRouter)
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware! Try adding on /users or /posts to the url to start seeing some data!</h2>`)
 });
 
 //custom middleware
